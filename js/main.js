d4e7df73bae245f9fe1003ba1827ac85bb83ebf5
Lucas Moe <deboramour4@gmail.com>
2019-02-05 16:33:28 -0300

    Desafio Mapa

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 $(document).ready(function() {
 	
+	$( "#imgPeca1, #imgPeca2, #imgPeca3").draggable({
+       cursor: "grabbing",
+
+        //snap: true,
+       scroll:false,
+       drag: function( event, ui ) {
+        console.log('aaaaaa');
+       }
+    });
+
+    $("#imgPeca1x").droppable({
+    	accept: "#imgPeca1",
+        drop: function( event, ui ) {
+          //alert('aaaaaa');
+          $( "#imgPeca1x" ).draggable( "disable" );
+          }
+      })
+
+     $("#imgPeca2x").droppable({
+    	accept: "#imgPeca2",
+        drop: function( event, ui ) {
+          //alert('aaaaaa');
+          $( "#imgPeca2x" ).draggable( "disable" );
+          }
+      })
+
+      $("#imgPeca3x").droppable({
+    	accept: "#imgPeca3",
+        drop: function( event, ui ) {
+          //alert('aaaaaa');
+          $( "#imgPeca3x" ).draggable( "disable" );
+          }
+      })
+
+
+
 });
