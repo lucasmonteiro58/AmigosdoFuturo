3fdd35d703ea795ede735a27b1599a5cb74049d9
Lucas Monteiro Ribeiro <lucas.monteiro58@gmail.com>
2019-02-06 16:50:45 -0300

    popup nas pecas e localizacao certa no mapa

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 	
 	$( "#imgPeca1, #imgPeca2, #imgPeca3").draggable({
        cursor: "grabbing",
@@ -13,8 +17,12 @@ $(document).ready(function() {
     $("#imgPeca1x").droppable({
     	accept: "#imgPeca1",
         drop: function( event, ui ) {
-          alert('aaaaaa');
-          $( "#imgPeca1x" ).draggable( "disable" );
+          alert('xxxxxxxx');
+          document.getElementById("imgPeca1").style.left = "0px";
+          document.getElementById("imgPeca1").style.top = "0px";
+          document.getElementById("imgPeca1").style.marginLeft = "70.25%";
+          document.getElementById("imgPeca1").style.marginTop = "55.42%";
+          $( "#imgPeca1" ).draggable( "disable" );
           }
       })  
     
@@ -23,7 +31,12 @@ $(document).ready(function() {
     	accept: "#imgPeca2",
         drop: function( event, ui ) {
           alert('aaaaaa');
-          $( "#imgPeca2x" ).draggable( "disable" );
+          document.getElementById("imgPeca2").style.left = "0px";
+          document.getElementById("imgPeca2").style.top = "0px";
+          document.getElementById("imgPeca2").style.marginLeft = "61.27%";
+          document.getElementById("imgPeca2").style.marginTop = "26.55%";
+
+          $( "#imgPeca2" ).draggable( "disable" );
           }
       })
 
@@ -31,7 +44,11 @@ $(document).ready(function() {
     	accept: "#imgPeca3",
         drop: function( event, ui ) {
           alert('aaaaaa');
-          $( "#imgPeca3x" ).draggable( "disable" );
+          document.getElementById("imgPeca3").style.left = "0px";
+          document.getElementById("imgPeca3").style.top = "0px";
+          document.getElementById("imgPeca3").style.marginLeft = "60%";
+          document.getElementById("imgPeca3").style.marginTop = "10.1%";
+          $( "#imgPeca3" ).draggable( "disable" );
           }
       })
 
