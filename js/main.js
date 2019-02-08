1afba5e55505da2f70b03bd5aa73f1143d1354b3
Débora Moura <deboramoura@alu.ufc.br>
2019-02-07 23:22:44 -0300

    Form dicamico com JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 // Modal fulscreen -----------------------
 $('#myModal').modal('show')
 
@@ -20,6 +19,57 @@ function openFullscreen() {
     elem.msRequestFullscreen();
   }
 }
+
+// Form  ----------------------------
+var question_number = 0 //first question
+createQuestion(questions_texts[question_number],"questions-next","Pronto")
+
+function nextQuestion() {
+  question_number = question_number+1
+  // console.log(questions_texts.length+1)
+  if (question_number == questions_texts.length-1) {
+    createQuestion(questions_texts[question_number],"questions-submit","Enviar")
+  } else {
+    createQuestion(questions_texts[question_number],"questions-next","Pronto")
+  }
+}
+function createQuestion(question,button_id,button_text) {
+  //Form reusable div
+  $(".form-question .content").html("<li><h4>"+question["title"]+"</h4></li>"+
+  "<li><form><input id='"+question["input_id"]+"' type='"+question["input_type"]+"' name='"+question["input_name"]+"' placeholder='"+question["placeholder"]+"'></form></li>"+
+  "<li><button id='"+button_id+"' class='play-btn action'>"+button_text+"</button></li>")
+
+  if (question["input_id"]=="city-name") {
+    autocompleteCities()
+  }
+  if (button_id=="questions-submit") {
+    submitButtonClick()
+    console.log("submit")
+  } else {
+    nextButtonClick()
+    console.log("next")
+  }
+}
+function autocompleteCities() {
+  $("#city-name").autocomplete({
+    source: available_cities
+  });
+}
+function nextButtonClick() {
+  $("#questions-next").click(function () {
+    nextQuestion()
+  })
+}
+function submitButtonClick() {
+  $("#questions-submit").click(function () {
+    submitQuestions()
+  })
+}
+function submitQuestions() {
+  alert("submit to server here...")
+  // submit to server here...
+}
+
 
 // Mapa ----------------------------
 $(function () {
