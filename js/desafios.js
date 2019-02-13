af11d793c134a2f1fafd1c46484039624f0db9cb
Lucas Monteiro Ribeiro <lucas.monteiro58@gmail.com>
2019-02-13 16:56:09 -0300

    desafio de plantar finalizado

--- js/desafios.js
+++ js/desafios.js
@@ -73,19 +73,19 @@ $("#lixometal").droppable({
       accept: "#metal1, #metal2",
          over: function( event, ui){
            $("#lixometal").attr('src',"img/lixeirametal_aberta.png");
-           $("#lixometal").css("margin-top", "73.6vh");
+           $("#lixometal").css("top", "70.4%");
 
         },
 
         out: function( event, ui){
            $("#lixometal").attr('src',"img/lixeirametal_fechada.png");
-           $("#lixometal").css("margin-top", "83vh");
+           $("#lixometal").css("top", "80%");
 
         },
 
         drop: function( event, ui ) {         
           $("#lixometal").attr('src',"img/lixeirametal_fechada.png");
-          $("#lixometal").css("margin-top", "83vh"); 
+          $("#lixometal").css("top", "80%"); 
           var draggableId = ui.draggable.attr("id");
           draggableId= '#'+ draggableId;    
           $(draggableId).css("display", "none");
@@ -97,19 +97,19 @@ $("#lixopapel").droppable({
       accept: "#papel1, #papel2",
        over: function( event, ui){
            $("#lixopapel").attr('src',"img/lixeirapapel_aberta.png");
-           $("#lixopapel").css("margin-top", "73.6vh");
+           $("#lixopapel").css("top", "70.4%");
 
         },
 
         out: function( event, ui){
            $("#lixopapel").attr('src',"img/lixeirapapel_fechada.png");
-           $("#lixopapel").css("margin-top", "83vh");
+           $("#lixopapel").css("top", "80%");
 
         },
 
         drop: function( event, ui ) {        
           $("#lixopapel").attr('src',"img/lixeirapapel_fechada.png");
-          $("#lixopapel").css("margin-top", "83vh");
+          $("#lixopapel").css("top", "80%");
           var draggableId = ui.draggable.attr("id");
           draggableId= '#'+ draggableId;    
           $(draggableId).css("display", "none");          
@@ -120,19 +120,19 @@ $("#lixoplastico").droppable({
       accept: "#plastico1, #plastico2",
         over: function( event, ui){
            $("#lixoplastico").attr('src',"img/lixeiraplastico_aberta.png");
-           $("#lixoplastico").css("margin-top", "73.8vh");
+           $("#lixoplastico").css("top", "70.4%");
 
         },
 
         out: function( event, ui){
            $("#lixoplastico").attr('src',"img/lixeiraplastico_fechada.png");
-           $("#lixoplastico").css("margin-top", "83vh");
+           $("#lixoplastico").css("top", "80%");
 
         },
 
         drop: function( event, ui ) {               
           $("#lixoplastico").attr('src',"img/lixeiraplastico_fechada.png");
-          $("#lixoplastico").css("margin-top", "83vh"); 
+          $("#lixoplastico").css("top", "80%"); 
           var draggableId = ui.draggable.attr("id");
           draggableId= '#'+ draggableId;    
           $(draggableId).css("display", "none");         
@@ -143,19 +143,19 @@ $("#lixovidro").droppable({
       accept: "#vidro1, #vidro2",
         over: function( event, ui){
            $("#lixovidro").attr('src',"img/lixeiravidro_aberta.png");
-           $("#lixovidro").css("margin-top", "73.6vh");
+           $("#lixovidro").css("top", "70.4%");
 
         },
 
         out: function( event, ui){
            $("#lixovidro").attr('src',"img/lixeiravidro_fechada.png");
-           $("#lixovidro").css("margin-top", "83vh");
+           $("#lixovidro").css("top", "80%");
 
         },
 
         drop: function( event, ui ) {         
           $("#lixovidro").attr('src',"img/lixeiravidro_fechada.png");
-          $("#lixovidro").css("margin-top", "83vh"); 
+          $("#lixovidro").css("top", "80%"); 
           var draggableId = ui.draggable.attr("id");
           draggableId= '#'+ draggableId;    
           $(draggableId).css("display", "none");         
@@ -166,19 +166,19 @@ $("#lixoorganico").droppable({
       accept: "#organico1, #organico2",
          over: function( event, ui){
            $("#lixoorganico").attr('src',"img/lixeiraorganico_aberta.png");
-           $("#lixoorganico").css("margin-top", "73.6vh");
+           $("#lixoorganico").css("top", "70.4%");
 
         },
 
         out: function( event, ui){
            $("#lixoorganico").attr('src',"img/lixeiraorganico_fechada.png");
-           $("#lixoorganico").css("margin-top", "83vh");
+           $("#lixoorganico").css("top", "80%");
 
         },
 
         drop: function( event, ui ) {                    
           $("#lixoorganico").attr('src',"img/lixeiraorganico_fechada.png");
-          $("#lixoorganico").css("margin-top", "83vh"); 
+          $("#lixoorganico").css("top", "80%"); 
           var draggableId = ui.draggable.attr("id");
           draggableId= '#'+ draggableId;    
           $(draggableId).css("display", "none");         
@@ -190,20 +190,23 @@ $("#lixoorganico").droppable({
 $('#buraco1').click(function(){
   if ($('#buraco1').attr('src')== "img/buraco.png"){
     $("#buraco1").attr('src', "");  
-    $("#buraco1").attr('src',"img/plantinha0.png");
-    $("#buraco1").css("margin-top", "50vh");
+    $("#buraco1").attr('src',"img/cenoura1.png");
+    $("#buraco1").css("top", "66%");
     $("#buraco1").removeClass('bcr1');
     $("#buraco1").addClass('bc1');
+
   }
 
-  else if($('#buraco1').attr('src')== "img/plantinha0.png"){
-    $("#buraco1").attr('src',"img/plantinha.png");
-    $("#buraco1").css("margin-top", "50vh");
+  else if($('#buraco1').attr('src')== "img/cenoura1.png"){
+    $("#buraco1").attr('src', "");
+    $("#buraco1").attr('src',"img/cenoura2.png");
+    $("#buraco1").css("top", "60%");
     $("#buraco1").removeClass('bc1');
     $("#buraco1").addClass('brcp1');
-  } else if ($('#buraco1').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco1').attr('src')== "img/cenoura2.png") {
     alert("aaaaa");
     $("#buraco1").attr('src',"");
+    $('#cenouracesta1').css('display', 'block');
 
   }  
 
@@ -213,20 +216,22 @@ $('#buraco1').click(function(){
 $('#buraco2').click(function(){
     if ($('#buraco2').attr('src')== "img/buraco.png"){
       $("#buraco2").attr('src', ""); 
-      $("#buraco2").attr('src',"img/plantinha0.png");
-      $("#buraco2").css("margin-top", "50vh");
+      $("#buraco2").attr('src',"img/cenoura1.png");
+      $("#buraco2").css("top", "71%");
       $("#buraco2").removeClass('bcr2');
       $("#buraco2").addClass('bc2');
   }
 
-  else if($('#buraco2').attr('src')== "img/plantinha0.png"){
-    $("#buraco2").attr('src',"img/plantinha.png");
-    $("#buraco2").css("margin-top", "50vh");
+  else if($('#buraco2').attr('src')== "img/cenoura1.png"){
+    $("#buraco2").attr('src', "");
+    $("#buraco2").attr('src',"img/cenoura2.png");
+    $("#buraco2").css("top", "66%");
     $("#buraco2").removeClass('bc2');
     $("#buraco2").addClass('brcp2');
-  } else if ($('#buraco2').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco2').attr('src')== "img/cenoura2.png") {
     alert("aaaaa");
     $("#buraco2").attr('src',"");
+    $('#cenouracesta2').css('display', 'block');
 
   }
    
@@ -235,20 +240,22 @@ $('#buraco2').click(function(){
 $('#buraco3').click(function(){
     if ($('#buraco3').attr('src')== "img/buraco.png"){
       $("#buraco3").attr('src', ""); 
-      $("#buraco3").attr('src',"img/plantinha0.png");
-      $("#buraco3").css("margin-top", "50vh");
+      $("#buraco3").attr('src',"img/tomate1.png");
+      $("#buraco3").css("top", "61%");
       $("#buraco3").removeClass('bcr3');
       $("#buraco3").addClass('bc3');
   }
 
-  else if($('#buraco3').attr('src')== "img/plantinha0.png"){
-    $("#buraco3").attr('src',"img/plantinha.png");
-    $("#buraco3").css("margin-top", "50vh");
+  else if($('#buraco3').attr('src')== "img/tomate1.png"){
+    $("#buraco3").attr('src', "");
+    $("#buraco3").attr('src',"img/tomate2.png");
+    $("#buraco3").css("top", "54%");
     $("#buraco3").removeClass('bc3');
     $("#buraco3").addClass('brcp3');
-  } else if ($('#buraco3').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco3').attr('src')== "img/tomate2.png") {
     alert("aaaaa");
     $("#buraco3").attr('src',"");
+    $('#tomatecesta1').css('display', 'block');
 
   }
 });
@@ -256,20 +263,22 @@ $('#buraco3').click(function(){
 $('#buraco4').click(function(){
     if ($('#buraco4').attr('src')== "img/buraco.png"){
       $("#buraco4").attr('src', ""); 
-      $("#buraco4").attr('src',"img/plantinha0.png");
-      $("#buraco4").css("margin-top", "65vh");
+      $("#buraco4").attr('src',"img/tomate1.png");
+      $("#buraco4").css("top", "68%");
       $("#buraco4").removeClass('bcr4');
       $("#buraco4").addClass('bc4');
   }
 
-  else if($('#buraco4').attr('src')== "img/plantinha0.png"){
-    $("#buraco4").attr('src',"img/plantinha.png");
-    $("#buraco4").css("margin-top", "65vh");
+  else if($('#buraco4').attr('src')== "img/tomate1.png"){
+    $("#buraco4").attr('src', "");
+    $("#buraco4").attr('src',"img/tomate2.png");
+    $("#buraco4").css("top", "62%");
     $("#buraco4").removeClass('bc4');
     $("#buraco4").addClass('brcp4');
-  } else if ($('#buraco4').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco4').attr('src')== "img/tomate2.png") {
     alert("aaaaa");
     $("#buraco4").attr('src',"");
+    $('#tomatecesta2').css('display', 'block');
 
   }
 });
@@ -277,20 +286,22 @@ $('#buraco4').click(function(){
 $('#buraco5').click(function(){
     if ($('#buraco5').attr('src')== "img/buraco.png"){
       $("#buraco5").attr('src', ""); 
-      $("#buraco5").attr('src',"img/plantinha0.png");
-      $("#buraco5").css("margin-top", "65vh");
+      $("#buraco5").attr('src',"img/alface1.png");
+      $("#buraco5").css("top", "67%");
       $("#buraco5").removeClass('bcr5');
       $("#buraco5").addClass('bc5');
   }
 
-  else if($('#buraco5').attr('src')== "img/plantinha0.png"){
-    $("#buraco5").attr('src',"img/plantinha.png");
-    $("#buraco5").css("margin-top", "65vh");
+  else if($('#buraco5').attr('src')== "img/alface1.png"){
+    $("#buraco5").attr('src', ""); 
+    $("#buraco5").attr('src',"img/alface2.png");
+    $("#buraco5").css("top", "64%");
     $("#buraco5").removeClass('bc5');
     $("#buraco5").addClass('brcp5');
-  } else if ($('#buraco5').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco5').attr('src')== "img/alface2.png") {
     alert("aaaaa");
     $("#buraco5").attr('src',"");
+    $('#alfacecesta1').css('display', 'block');
 
   }
 });
@@ -298,20 +309,22 @@ $('#buraco5').click(function(){
 $('#buraco6').click(function(){
     if ($('#buraco6').attr('src')== "img/buraco.png"){
       $("#buraco6").attr('src', ""); 
-      $("#buraco6").attr('src',"img/plantinha0.png");
-      $("#buraco6").css("margin-top", "65vh");
+      $("#buraco6").attr('src',"img/alface1.png");
+      $("#buraco6").css("top", "73%");
       $("#buraco6").removeClass('bcr6');
       $("#buraco6").addClass('bc6');
   }
 
-  else if($('#buraco6').attr('src')== "img/plantinha0.png"){
-    $("#buraco6").attr('src',"img/plantinha.png");
-    $("#buraco6").css("margin-top", "65vh");
+  else if($('#buraco6').attr('src')== "img/alface1.png"){
+    $("#buraco6").attr('src', ""); 
+    $("#buraco6").attr('src',"img/alface2.png"); 
+    $("#buraco6").css("top", "70%");
     $("#buraco6").removeClass('bc6');
     $("#buraco6").addClass('brcp6');
-  } else if ($('#buraco6').attr('src')== "img/plantinha.png") {
+  } else if ($('#buraco6').attr('src')== "img/alface2.png") {
     alert("aaaaa");
     $("#buraco6").attr('src',"");
+    $('#alfacecesta2').css('display', 'block');
 
   }
 });
