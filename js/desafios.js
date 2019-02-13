e3cba32777964844d7bb381e6c0e2556811c6d44
Lucas Monteiro <lucas.monteiro58@gmail.com>
2019-02-12 22:52:49 -0300

    Desafio lixo adicionado assets

--- js/desafios.js
+++ js/desafios.js
@@ -55,60 +55,133 @@ $(function () {
 
 // Desafio Lixo -----------------------------------------------------------------
 
-$( "#papel, #metal, #plastico, #vidro, #organico").draggable({
-
+$( "#papel1, #metal1, #plastico1, #vidro1, #organico1, #papel2, #metal2, #plastico2, #vidro2, #organico2").draggable({
+      revert: true,
+      revertDuration: 600,
        cursor: "grabbing",
+
 
 
         //snap: true,
        scroll:false,
        drag: function( event, ui ) {
-        console.log('aaaaaa');
+        
        }
     });
 
 $("#lixometal").droppable({
-      accept: "#metal",
-        drop: function( event, ui ) {
-          alert('xxxxxxxx');
-          $( "#imgPeca1" ).draggable( "disable" );
-          document.getElementById("metal").style.display = "none";
+      accept: "#metal1, #metal2",
+         over: function( event, ui){
+           $("#lixometal").attr('src',"img/lixeirametal_aberta.png");
+           $("#lixometal").css("margin-top", "73.6vh");
+
+        },
+
+        out: function( event, ui){
+           $("#lixometal").attr('src',"img/lixeirametal_fechada.png");
+           $("#lixometal").css("margin-top", "83vh");
+
+        },
+
+        drop: function( event, ui ) {         
+          $("#lixometal").attr('src',"img/lixeirametal_fechada.png");
+          $("#lixometal").css("margin-top", "83vh"); 
+          var draggableId = ui.draggable.attr("id");
+          draggableId= '#'+ draggableId;    
+          $(draggableId).css("display", "none");
+
           }
       });
 
 $("#lixopapel").droppable({
-      accept: "#papel",
-        drop: function( event, ui ) {
-          alert('xxxxxxxx');
-          $( "#imgPeca1" ).draggable( "disable" );
-          document.getElementById("papel").style.display = "none";
+      accept: "#papel1, #papel2",
+       over: function( event, ui){
+           $("#lixopapel").attr('src',"img/lixeirapapel_aberta.png");
+           $("#lixopapel").css("margin-top", "73.6vh");
+
+        },
+
+        out: function( event, ui){
+           $("#lixopapel").attr('src',"img/lixeirapapel_fechada.png");
+           $("#lixopapel").css("margin-top", "83vh");
+
+        },
+
+        drop: function( event, ui ) {        
+          $("#lixopapel").attr('src',"img/lixeirapapel_fechada.png");
+          $("#lixopapel").css("margin-top", "83vh");
+          var draggableId = ui.draggable.attr("id");
+          draggableId= '#'+ draggableId;    
+          $(draggableId).css("display", "none");          
           }
       });
 
 $("#lixoplastico").droppable({
-      accept: "#plastico",
-        drop: function( event, ui ) {
-          alert('xxxxxxxx');
-          $( "#imgPeca1" ).draggable( "disable" );
-          document.getElementById("plastico").style.display = "none";
+      accept: "#plastico1, #plastico2",
+        over: function( event, ui){
+           $("#lixoplastico").attr('src',"img/lixeiraplastico_aberta.png");
+           $("#lixoplastico").css("margin-top", "73.8vh");
+
+        },
+
+        out: function( event, ui){
+           $("#lixoplastico").attr('src',"img/lixeiraplastico_fechada.png");
+           $("#lixoplastico").css("margin-top", "83vh");
+
+        },
+
+        drop: function( event, ui ) {               
+          $("#lixoplastico").attr('src',"img/lixeiraplastico_fechada.png");
+          $("#lixoplastico").css("margin-top", "83vh"); 
+          var draggableId = ui.draggable.attr("id");
+          draggableId= '#'+ draggableId;    
+          $(draggableId).css("display", "none");         
           }
       });
 
 $("#lixovidro").droppable({
-      accept: "#vidro",
-        drop: function( event, ui ) {
-          alert('xxxxxxxx');
-          $( "#imgPeca1" ).draggable( "disable" );
-          document.getElementById("vidro").style.display = "none";
+      accept: "#vidro1, #vidro2",
+        over: function( event, ui){
+           $("#lixovidro").attr('src',"img/lixeiravidro_aberta.png");
+           $("#lixovidro").css("margin-top", "73.6vh");
+
+        },
+
+        out: function( event, ui){
+           $("#lixovidro").attr('src',"img/lixeiravidro_fechada.png");
+           $("#lixovidro").css("margin-top", "83vh");
+
+        },
+
+        drop: function( event, ui ) {         
+          $("#lixovidro").attr('src',"img/lixeiravidro_fechada.png");
+          $("#lixovidro").css("margin-top", "83vh"); 
+          var draggableId = ui.draggable.attr("id");
+          draggableId= '#'+ draggableId;    
+          $(draggableId).css("display", "none");         
           }
       });
 
 $("#lixoorganico").droppable({
-      accept: "#organico",
-        drop: function( event, ui ) {
-          alert('xxxxxxxx');
-          $( "#imgPeca1" ).draggable( "disable" );
-          document.getElementById("organico").style.display = "none";
+      accept: "#organico1, #organico2",
+         over: function( event, ui){
+           $("#lixoorganico").attr('src',"img/lixeiraorganico_aberta.png");
+           $("#lixoorganico").css("margin-top", "73.6vh");
+
+        },
+
+        out: function( event, ui){
+           $("#lixoorganico").attr('src',"img/lixeiraorganico_fechada.png");
+           $("#lixoorganico").css("margin-top", "83vh");
+
+        },
+
+        drop: function( event, ui ) {                    
+          $("#lixoorganico").attr('src',"img/lixeiraorganico_fechada.png");
+          $("#lixoorganico").css("margin-top", "83vh"); 
+          var draggableId = ui.draggable.attr("id");
+          draggableId= '#'+ draggableId;    
+          $(draggableId).css("display", "none");         
           }
       });
 
